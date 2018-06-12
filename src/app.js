968f5ed850c3fdb0159b746c34a51241c1a14b4d
328553173@qq.com <xiaoliang.yu@xiaoliangyudeMacBook-Pro.local>
2018-06-12 20:16:18 +0800

    add

--- src/app.js
+++ src/app.js
@@ -6,4 +6,4 @@ import {render} from 'react-dom';
 import initialSize from './utils/flexiable';
 import Home from './containers/home/index.js';
 initialSize(window, window['lib'] || (window['lib'] = {}));
-render(<Home/>,document.getElementById('app'));
+render(<Home/>,document.getElementById('app'))
